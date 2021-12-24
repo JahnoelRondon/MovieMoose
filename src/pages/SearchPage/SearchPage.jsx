9287b123a99f4cb7909542b7bc87649d80bc1d86
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-12-23 21:31:52 -0500

    conditional rendering for movies with no poster paths, set to null

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
 export const SearchPage = ({searchData}) => {
-    console.log('search page: ', searchData)
     return (
         <div>
             {
                 searchData.map(movie => (
+                    movie.poster_path ? 
                     <div key={movie.id}>
                         <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} width='200px'/>
                     </div>
+                    :
+                    null
                 ))
             }
 
