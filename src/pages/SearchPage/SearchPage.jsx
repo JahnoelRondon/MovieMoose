ab60a650da050105e3f44fd98eb2c6536fc3ae67
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-12-24 11:31:53 -0500

    added styling for mobile when rendering movies and content, turned movie into flex grid as well as its content, made the index.css body background black, recolor header

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import './searchpage.css'
 
 export const SearchPage = ({searchData}) => {
+    console.log(searchData);
     return (
-        <div>
+        <div className='searchContainer'>
             {
                 searchData.map(movie => (
+                    // if poster movie path exists render otherwise render no value
                     movie.poster_path ? 
-                    <div key={movie.id}>
-                        <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} width='200px'/>
+
+                    <div className='movie' key={movie.id}>
+                        <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}/>
+                        <div className='movieContent'>
+                            <p>{movie.title}<br/><small>{movie.release_date}</small></p>
+                            <button>add to watchlist</button>
+                            <button>already watched</button>                            
+                        </div>
+
                     </div>
                     :
                     null
