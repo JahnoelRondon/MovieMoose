f36ab3f3e6d59493cbe19f7245447173524a7578
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-12-23 21:23:23 -0500

    added rendering of poster paths for testing

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -4,7 +4,14 @@ export const SearchPage = ({searchData}) => {
     console.log('search page: ', searchData)
     return (
         <div>
-            <h1>Search page</h1>
+            {
+                searchData.map(movie => (
+                    <div key={movie.id}>
+                        <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} width='200px'/>
+                    </div>
+                ))
+            }
+
         </div>
     )
 }
