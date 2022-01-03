79036d150de0162e3cc663288c70bc3a3ca27173
JahnoelRondon <jahnoelrondon03@gmail.com>
2022-01-02 21:18:03 -0500

    added styling for poster delete buttons and hidden display until on hover

--- src/pages/WatchList/WatchList.jsx
+++ src/pages/WatchList/WatchList.jsx
@@ -1,7 +1,7 @@
 import './watchlist.css'
 import React, {useContext} from 'react'
 import {GlobalContext} from './../../context/GlobalState'
-
+import { FaTrashAlt } from "react-icons/fa";
 
 export const WatchList = () => {
     const {watchlist, removeFromWatchlist} = useContext(GlobalContext)
@@ -13,7 +13,7 @@ export const WatchList = () => {
 
                     <div className='movieWatch' key={movie.id}>
                         <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={`${movie.title} poster`}/>
-                        <button onClick={() => removeFromWatchlist(movie.id)}>Delete</button>
+                        <button onClick={() => removeFromWatchlist(movie.id)}><FaTrashAlt/></button>
                     </div>
                 ))     
             :
